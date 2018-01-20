e783c035aa843dddfeb400f7a4dd8812b0b98b41
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2018-01-20 22:39:56 +0100

    0.0.17

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
 import { Value } from './Value';
 import { ValueComputed } from './ValueComputed';
 import { ValueConnection } from './ValueConnection';
+import { transaction } from './transaction';
 
 export {
     Value,
     ValueComputed,
-    ValueConnection
+    ValueConnection,
+    transaction
 };
